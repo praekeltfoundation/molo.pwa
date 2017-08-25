1cb02e2d2753e2e1e01562169cc053a9b15e90a5
Johan Bouwer <projectmushroom@me.com>
2017-08-25 12:30:11 +0200

    more cleanup

--- molo/pwa/templates/serviceworker.js
+++ molo/pwa/templates/serviceworker.js
@@ -1,1 +1,3 @@
-// Set the PWA_SERVICE_WORKER_PATH variable in settings base.py
+// Empty Service Worker implementation.
+// To use your own Service Worker, set the PWA_SERVICE_WORKER_PATH variable in
+// base.py
